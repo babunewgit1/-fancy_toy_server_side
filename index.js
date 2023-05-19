f4522ed7ace3ccbaeefeb5c02357ba29aa9de3e6
babunewgit1 <babunewx11@gmail.com>
2023-05-19 21:12:22 +0600

    update

---
+++ index.js
@@ -0,0 +1,53 @@
+const { MongoClient, ServerApiVersion } = require("mongodb");
+const express = require("express");
+const app = express();
+const port = process.env.PORT || 5000;
+const cors = require("cors");
+require("dotenv").config();
+
+// middleware
+app.use(cors());
+app.use(express.json());
+
+//mongobd setup
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster-1.vbj8kjp.mongodb.net/?retryWrites=true&w=majority`;
+
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  },
+});
+
+async function run() {
+  try {
+    await client.connect();
+    const database = client.db("fancytoy").collection("fancytoyproduct");
+
+    // routes defines
+    app.post("/addtoy", async (req, res) => {
+      const toy = req.body;
+      const result = await database.insertOne(toy);
+      res.send(result);
+    });
+
+    await client.db("admin").command({ ping: 1 });
+    console.log(
+      "Pinged your deployment. You successfully connected to MongoDB!"
+    );
+  } finally {
+    // await client.close();
+  }
+}
+run().catch(console.dir);
+
+//routes
+app.get("/", (req, res) => {
+  res.send("Hello World!");
+});
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`);
+});
