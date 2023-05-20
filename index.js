6f9dbd2a50242a58037458c49d1881d3adb5e6f1
babunewgit1 <babunewx11@gmail.com>
2023-05-21 04:02:40 +0600

    sort functionality created

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-const { MongoClient, ServerApiVersion } = require("mongodb");
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 const express = require("express");
 const app = express();
 const port = process.env.PORT || 5000;
@@ -33,9 +33,61 @@ async function run() {
       res.send(result);
     });
 
+    app.get("/kidstoy", async (req, res) => {
+      const result = await database.find({}).limit(20).toArray();
+      res.send(result);
+    });
+
+    app.get("/search/:text", async (req, res) => {
+      const element = req.params.text;
+      const result = await database
+        .find({ toy_name: { $regex: element, $options: "i" } })
+        .toArray();
+      res.send(result);
+    });
+
     app.get("/alltoy/:cata", async (req, res) => {
       const cata = req.params.cata;
       const result = await database.find({ sub_cata: cata }).toArray();
+      res.send(result);
+    });
+
+    app.get("/toy/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
+      const result = await database.findOne(query);
+      res.send(result);
+    });
+
+    app.get("/mytoy/:email", async (req, res) => {
+      const mail = req.params.email;
+      const sort = req.query.sort;
+      const result = await database
+        .find({ seller_email: mail })
+        .sort({ price: sort })
+        .toArray();
+      res.send(result);
+    });
+
+    // app.put("/update/:id", async (req, res) => {
+    //   const updateId = req.params.id;
+    //   const docs = req.body;
+      const filter = { _id: new ObjectId(updateId) };
+      const updateDoc = {
+        $set: {
+          price: docs.price,
+          quentity: docs.quentity,
+          discription: docs.discription,
+        },
+      };
+      const result = await database.updateOne(filter, updateDoc);
+      res.send(result);
+    });
+
+    app.delete("/delete/:id", async (req, res) => {
+      const deletedId = req.params.id;
+      const query = { _id: new ObjectId(deletedId) };
+      const result = await database.deleteOne(query);
       res.send(result);
     });
 
